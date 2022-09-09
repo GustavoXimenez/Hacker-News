ef0c323e9ce4724d155369a0c91322662ed724e4
AzureAD\GustavoRafaelJimenez <gjimenez@biometriaaplicada.com>
2022-09-09 10:38:36 -0500

    Implement MVVM in the project.

---
+++ app/src/main/java/com/grjt/hackernews/model/NewsModel.kt
@@ -0,0 +1,11 @@
+package com.grjt.hackernews.model
+
+data class NewsModel(
+    val createAt: String = "",
+    val title: String = "",
+    val url: String = "",
+    val author: String = "",
+    val storyId: String = "",
+    val storyTitle: String = "",
+    val storyUrl: String = ""
+)
