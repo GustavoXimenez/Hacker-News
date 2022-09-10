8cf1895324251fb1b3f48b954412c353c417be53
AzureAD\GustavoRafaelJimenez <gjimenez@biometriaaplicada.com>
2022-09-10 01:45:59 -0500

    Implement UI List News Hackers

--- app/src/main/java/com/grjt/hackernews/data/network/NewsService.kt
+++ app/src/main/java/com/grjt/hackernews/data/network/NewsService.kt
@@ -4,6 +4,7 @@ import android.util.Log
 import com.grjt.hackernews.core.RetrofitHelper
 import com.grjt.hackernews.data.local.NewsProvider
 import com.grjt.hackernews.data.model.NewsModel
+import com.grjt.hackernews.data.model.ResponseNews
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 
@@ -11,13 +12,13 @@ class NewsService {
 
     private val retrofit = RetrofitHelper.getRetrofit()
 
-    suspend fun getNews():List<NewsModel> {
+    suspend fun getNews():ResponseNews {
         return withContext(Dispatchers.IO) {
             try {
                 val response = retrofit.create(NewsApiClient::class.java).getAllNews()
-                response.body() ?: emptyList()
+                response.body() ?: ResponseNews()
             } catch (e: Exception) {
-                NewsProvider.news
+                ResponseNews()
             }
         }
     }
--- app/src/main/java/com/grjt/hackernews/ui/viewmodel/NewsViewModel.kt
+++ app/src/main/java/com/grjt/hackernews/ui/viewmodel/NewsViewModel.kt
@@ -2,20 +2,24 @@ package com.grjt.hackernews.ui.viewmodel
 
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.liveData
 import androidx.lifecycle.viewModelScope
+import com.grjt.hackernews.core.Resource
 import com.grjt.hackernews.data.model.NewsModel
 import com.grjt.hackernews.domain.GetNewsUseCase
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 
 class NewsViewModel : ViewModel() {
 
     val newsUseCase = GetNewsUseCase()
-    val newsModel = MutableLiveData<List<NewsModel>>()
 
-    fun getNews() {
-        viewModelScope.launch {
-            val currentNews = newsUseCase()
-            newsModel.postValue(currentNews)
+    fun getNews() = liveData(Dispatchers.IO) {
+        emit(Resource.Loading())
+        try {
+            emit(Resource.Success(newsUseCase()))
+        } catch (e: Exception) {
+            emit(Resource.Failure(e))
         }
     }
 
